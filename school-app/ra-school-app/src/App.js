109c8a73328c63efdce2c5795f9513a1d7717177
Cátia Antunes <catiafantunes@gmail.com>
2023-01-15 14:13:59 +0000

    Name change

--- school-app/ra-school-app/src/App.js
+++ school-app/ra-school-app/src/App.js
@@ -1,6 +1,6 @@
 import {Admin, Resource} from "react-admin";
 import lb4Provider from "react-admin-lb4";
-import { AlunoCreate, CursoCreate, DisciplinaCreate, ProfessorCreate } from "./Create";
+import { AlunoCreate, CursoCreate, DisciplinaCreate, ProfessorCreate } from "./Creates";
 import { AlunoEdit, CursoEdit, DisciplinaEdit, ProfessorEdit } from "./Edits";
 import { AlunoList, CursoList, DisciplinaList, ProfessorList } from "./Lists";
 
@@ -8,10 +8,27 @@ const dataProvider = lb4Provider("http://localhost:3000/");
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="alunos" list={AlunoList} edit={AlunoEdit} create={AlunoCreate}/>
-    <Resource name="cursos" list={CursoList} edit={CursoEdit} create={CursoCreate}/>
-    <Resource name="disciplinas" list={DisciplinaList} edit={DisciplinaEdit} create={DisciplinaCreate}/>
-    <Resource name="professors" list={ProfessorList} edit={ProfessorEdit} create={ProfessorCreate}/>
+    <Resource   name="alunos" 
+                list={AlunoList} 
+                edit={AlunoEdit} 
+                create={AlunoCreate}
+    />
+    <Resource   name="cursos" 
+                list={CursoList} 
+                edit={CursoEdit} 
+                create={CursoCreate}
+    />
+    <Resource   name="disciplinas" 
+                list={DisciplinaList} 
+                edit={DisciplinaEdit} 
+                create={DisciplinaCreate}
+    />
+    <Resource   name="professors" 
+                options={{label: "Professores"}} 
+                list={ProfessorList} 
+                edit={ProfessorEdit} 
+                create={ProfessorCreate}
+    />
   </Admin>
 );
 
